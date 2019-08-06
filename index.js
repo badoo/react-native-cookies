ed9b0f8494071eab3c2d24702c33bb8f7c393800
Nikhil Verma <nikhil.verma@corp.badoo.com>
2019-08-06 10:57:36 +0100

    Consistent output is necessary

--- index.js
+++ index.js
@@ -7,30 +7,46 @@ const RNCookieManagerAndroid = NativeModules.RNCookieManagerAndroid;
 let CookieManager;
 
 if (Platform.OS === 'ios') {
-    invariant(RNCookieManagerIOS,
-        'react-native-cookies: Add RNCookieManagerIOS.h and RNCookieManagerIOS.m to your Xcode project');
+    invariant(
+        RNCookieManagerIOS,
+        'react-native-cookies: Add RNCookieManagerIOS.h and RNCookieManagerIOS.m to your Xcode project'
+    );
     CookieManager = RNCookieManagerIOS;
 } else if (Platform.OS === 'android') {
-    invariant(RNCookieManagerAndroid,
-        'react-native-cookies: Import libraries to android "react-native link react-native-cookies"');
-    CookieManager = RNCookieManagerAndroid;
+    invariant(
+        RNCookieManagerAndroid,
+        'react-native-cookies: Import libraries to android "react-native link react-native-cookies"'
+    );
+    CookieManager = {
+        getAll: (useWebKit = false) => RNCookieManagerAndroid.getAll(useWebKit),
+        clearAll: (useWebKit = false) => RNCookieManagerAndroid.clearAll(useWebKit),
+        get: (url, useWebKit = false) => RNCookieManagerAndroid.get(url, useWebKit).then(cookieMap => Object.keys(cookieMap).reduce((acc, key) => {
+            acc[key] = {
+                name: key,
+                value: cookieMap[key],
+                domain: null,
+                path: null
+            };
+            return acc;
+        }, {})),
+        set: (cookie, useWebKit = false) => RNCookieManagerAndroid.set(cookie, useWebKit)
+    };
 } else {
-    invariant(CookieManager, 'react-native-cookies: Invalid platform. This library only supports Android and iOS.');
+    invariant(
+        CookieManager,
+        'react-native-cookies: Invalid platform. This library only supports Android and iOS.'
+    );
 }
 
-const functions = [
-    'setFromResponse',
-    'getFromResponse',
-    'clearByName'
-];
+const functions = ['setFromResponse', 'getFromResponse', 'clearByName'];
 
 module.exports = {
-  getAll: (useWebKit = false) => CookieManager.getAll(useWebKit),
-  clearAll: (useWebKit = false) => CookieManager.clearAll(useWebKit),
-  get: (url, useWebKit = false) => CookieManager.get(url, useWebKit),
-  set: (cookie, useWebKit = false) => CookieManager.set(cookie, useWebKit),
+    getAll: (useWebKit = false) => CookieManager.getAll(useWebKit),
+    clearAll: (useWebKit = false) => CookieManager.clearAll(useWebKit),
+    get: (url, useWebKit = false) => CookieManager.get(url, useWebKit),
+    set: (cookie, useWebKit = false) => CookieManager.set(cookie, useWebKit)
 };
 
-for (var i = 0; i < functions.length; i++) {
+for (let i = 0; i < functions.length; i++) {
     module.exports[functions[i]] = CookieManager[functions[i]];
 }
